265cbdd1cdf88ca0fa621fa302b268a262616de5
enoki <h.enoki@stv-tech.co.jp>
2023-03-07 17:33:02 +0900

    ReorderableListViewに変更

--- lib/src/views/home_page.dart
+++ lib/src/views/home_page.dart
@@ -16,25 +16,53 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("ToDoリスト"),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.create_rounded),
-              onPressed: () => {},
+      appBar: AppBar(
+        title: const Text("ToDoリスト"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.create_rounded),
+            onPressed: () => {},
+          ),
+        ],
+      ),
+      // body: ListView.separated(
+      //   padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      //   itemCount: task.length,
+      //   separatorBuilder: (BuildContext context, int index) {
+      //     return const Divider(color: Colors.grey);
+      //   },
+      //   itemBuilder: (context, index) {
+      //     return TodoListItem(index: index, task: task[index]);
+      //   },
+      // ),
+      body: ReorderableListView.builder(
+        itemCount: task.length,
+        header: const AddTaskListTile(),
+        footer: const AddTaskListTile(),
+        itemBuilder: (context, index) {
+          return Dismissible(
+            // スワイプ禁止
+            direction: DismissDirection.none,
+            key: Key(task[index].title),
+            child: TodoListItem(
+              index: index,
+              task: task[index],
             ),
-          ],
-        ),
-        body: ListView.separated(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          itemCount: task.length,
-          separatorBuilder: (BuildContext context, int index) {
-            return const Divider(color: Colors.grey);
-          },
-          itemBuilder: (context, index) {
-            return TodoListItem(index: index, task: task[index]);
-          },
-        ));
+          );
+        },
+        // ドラッグ&ドロップ時のメソッド
+        onReorder: (oldIndex, newIndex) {
+          debugPrint("onReorder");
+          // setState(() {
+          //   if (newIndex > oldIndex) {
+          //     newIndex -= 1;
+          //   }
+          //   final item = _items.removeAt(oldIndex);
+          //   _items.insert(newIndex, item);
+          // });
+        },
+      ),
+    );
   }
 }
 
@@ -50,11 +78,47 @@ class TodoListItem extends StatelessWidget {
       onTap: () {
         debugPrint("onTap：$index");
       },
-      child: ListTile(
-        title: Text(task.title),
-        trailing: task.isCompleted
-            ? Icon(Icons.check, color: Theme.of(context).primaryColor)
-            : null,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+        ),
+        child: ListTile(
+          key: Key(task.title),
+          title: Text(task.title),
+          trailing: task.isCompleted
+              ? Icon(Icons.check, color: Theme.of(context).primaryColor)
+              : null,
+        ),
+      ),
+    );
+  }
+}
+
+class AddTaskListTile extends StatelessWidget {
+  const AddTaskListTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        debugPrint("AddTask");
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(color: Colors.grey),
+          ),
+        ),
+        child: const ListTile(
+          title: Text("タスクを追加"),
+          leading: Icon(Icons.add),
+        ),
       ),
     );
   }
